fac5e33efac8ae8972c2b20775689907acb8f965
Siim Laineste <siim.laineste@outlook.com>
2021-01-13 12:40:09 +0200

    concept

--- main.py
+++ main.py
@@ -1,11 +1,29 @@
 import json
 from subprocess import check_output
 
+
 checking = "dir"
+docloc = "cd ~/Documents"
+# copy directories
+# cp -r Documents USB
+def change():
 
-back = check_output(checking, shell=True, universal_newlines=True)
-print(back)
-with open('datadump.json', 'w')as json_file:
-        json.dump(back, json_file)
-    
+        change_to = check_output(docloc, shell=True, universal_newlines=True)
+        print(change_to)
+        contents()
 
+def contents():
+        back = check_output(checking, shell=True, universal_newlines=True)
+        with open('datadump.json', 'w')as json_file:
+                json.dump(back, json_file)
+                print(back)
+
+#def getContents():
+        #cop_command = "cp -r DOCUMENTS USB"
+        #cop_do = check_output(cop_command, shell=True, universal_newlines=True)
+
+
+def onExit():
+        print('Done!')
+
+change()
